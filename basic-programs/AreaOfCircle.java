867e647fe86a7d2c023ca940ded2314335a79ad0
ABHAY CHAUDHARY <abhaychaudhary343434@gmail.com>
2026-01-13 04:13:21 +0000

    Modified solutions

--- basic-programs/AreaOfCircle.java
+++ basic-programs/AreaOfCircle.java
@@ -1,11 +1,9 @@
 import java.util.*;
-public class Add{
-    public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
-        System.out.println("ENTER A NUMBER:");
-        double x=sc.nextDouble();
-        System.out.println("ENTER A NUMBER:");
-        double y=sc.nextDouble();
-        System.out.println("SUM OF THE GIVEN NUMBERS="+(x+y));
+public class AreaOfCircle{
+	public static void main(String args[]){
+		Scanner sc= new Scanner(System.in);
+		System.out.println("ENTER RADIUS:");
+		double x=sc.nextDouble();
+		System.out.println("AREA="+Math.PI*x*x);
 	}
 }
